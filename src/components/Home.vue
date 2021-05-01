0d3f1d97f6a399df7466c251a49df8d5023868ee
christopher <tomiwa_chris@yahoo.com>
2021-05-01 16:20:55 +0100

    implementing the user interface

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,29 @@
 <template>
-   <v-btn class="hidden-sm-and-down">Click Me</v-btn>
+<nav>
+ <v-toolbar flat app>
+  
+     <v-text-field 
+        class="hidden-md-and-up"
+          width="50"
+          label="Search Gallery..."
+          >
+          </v-text-field>
+          <span><v-btn plain 
+          class="hidden-md-and-up"
+          >search</v-btn></span>
+
+   
+       
+
+          
+         
+        
+  
+ </v-toolbar>
+ </nav>
+     
+        
+       
   
 </template>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,57 @@
 <template>
-<nav>
-    <v-toolbar flat app>
-    
-        <v-toolbar-title class="dark--text">
-            <span class="font-weight-light">AUTO</span>
-            <span>DRIVE</span>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn flat color="grey">
-            <span>Sign In</span>
-            <v-icon right>vpn_key</v-icon>
-        </v-btn>
-        <v-icon right @click="drawer=!drawer">menu</v-icon>
-             
-        </v-toolbar>
+  <nav>
+    <v-toolbar app>
+      <v-toolbar-title class="dark--text">
+        <span class="font-weight-light">AUTO</span>
+        <span><b>DRIVE</b></span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+        class="hidden-sm-and-down"
+        width="50"
+        label="Search Gallery..."
+      ></v-text-field>
+      <v-btn plain right class="hidden-sm-and-down">search</v-btn>
 
-        <v-navigation-drawer class="grey" v-model= "drawer" app>
-            <p>test</p>
-        </v-navigation-drawer>
-    </nav>   
+      <v-a>
+        <v-icon right large class="material-icons-round">person</v-icon> LOGIN
+      </v-a>
+      <v-tab>
+        <v-badge right color="black accent-7" content="0" overlap>
+          <v-icon large right>shopping_cart</v-icon>
+        </v-badge>
+      </v-tab>
+
+      <v-icon
+        medium
+        right
+        @click="drawer = !drawer"
+        class=" material-icons-round"
+        >menu</v-icon
+      >
+      <!-- hidden-md-and-up -->
+    </v-toolbar>
+
+    <v-navigation-drawer class="grey darken-4 " v-model="drawer" app>
+      <v-list v-for="link in links" :key="link.text" router :to="link.route">
+        <v-icon class="mx-5 white--text">{{ link.icon }}</v-icon>
+        <span class="white--text">{{ link.text }}</span>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
-export default{
-    data(){
-        return{
-            drawer: false
-        }
-    }
-
-}
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: "home", text: "Home", route: "/#" },
+        { icon: "account_box", text: "Account", route: "/#" },
+        { icon: "person", text: "Login", route: "/#" },
+      ],
+    };
+  },
+};
 </script>
